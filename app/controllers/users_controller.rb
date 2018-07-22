e9a8e2ea76ac21e191eedd43913227819bfcc5cd
Fortunat Mutunda <fotimutunda@gmail.com>
2018-07-23 00:47:17 +0300

    small fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: { id: @user.id },
+      render json: { id: @user.id, message: 'user registration successful' },
              status: :created,
              location: @user
     else
